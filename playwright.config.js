a4181ae475541f990274a9bb6e304249cf68e02d
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-07-18 17:05:52 -0500

    Some example tests Added

---
+++ playwright.config.js
@@ -0,0 +1,21 @@
+// playwright.config.js
+module.exports = {
+    // Look for test files in the "tests" directory, relative to this configuration file
+    testDir: 'tests',
+
+    // Each test is given 30 seconds
+    timeout: 30000,
+
+    // Forbid test.only on CI
+    forbidOnly: !!process.env.CI,
+
+    // Two retries for each test
+    retries: 2,
+
+    // Limit the number of workers on CI, use default locally
+    workers: process.env.CI ? 2 : undefined,
+
+    use: {
+        // Configure browser and context here
+    },
+};
---
+++ tests/example.spec.js
@@ -0,0 +1,12 @@
+// example.spec.js
+const { test, expect } = require('@playwright/test');
+
+test.beforeEach(async ({ context }) => {
+    // Block any css requests for each test in this file.
+    await context.route(/.css/, route => route.abort());
+});
+
+test('loads page without css', async ({ page }) => {
+    await page.goto('https://playwright.dev');
+    // ... test goes here
+});
